a27d15eecfcc319b8743385b657486457e8a80fb
mijm <mdmisbah27@gmail.com>
2020-08-27 17:25:23 +0600

    ch

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'helpdesk2k19@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'shopcom'),
+        'address' => env('MAIL_FROM_ADDRESS', ''),
+        'name' => env('MAIL_FROM_NAME', ''),
     ],
 
     /*
